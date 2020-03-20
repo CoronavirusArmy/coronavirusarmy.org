1191faaf215f18e85e0a493ec4cf6d8de66d8433
chano08 <christianraymondpalis@gmail.com>
2020-03-21 01:00:48 +0800

    integrate insync api endpoint

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,7 +6,7 @@ import auth from "./auth";
 import userInfo from "./user/info";
 import { list, latest, all } from "./user/list";
 import { list as initiatives, details, join } from "./initiatives";
-import { getDetails, getInitiatives } from "./user/volunteer";
+import { getDetails, getInitiatives, getTime, getScreenshots } from "./user/volunteer";
 
 export default () => {
     let router = Router();
@@ -25,7 +25,8 @@ export default () => {
     router.get("/initiatives", initiatives, respond, error);
     router.get("/initiative/:id", details, respond, error);
     router.put("/initiative/:id", passport.authenticate('jwt', { session : false }), isAuthenticated, join, respond, error);
-    router.get("/volunteer/:id", getDetails, getInitiatives, respond, error);
+    router.get("/volunteer/:id", getDetails, getTime, getInitiatives, respond, error);
+    router.get("/volunteer/:id/screenshots/:page", getDetails, getScreenshots, respond, error);
     
     return router;
 };
--- api/src/routes/user/volunteer.js
+++ api/src/routes/user/volunteer.js
@@ -1,39 +1,75 @@
+import dotenv from "dotenv";
+import request from "request";
+
 import User from "../../models/user";
 import Initiative from "../../models/initiative";
 
 import { toObjectId } from "../../helpers/utils";
 
+dotenv.config();
+
+const getInSyncData = (url, data) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            uri: `https://api.insync.team/coronavirus${url}`,
+            method: "POST",
+            json: { key: process.env.INSYNC_KEY, ...data }
+        };
+        request(options, function optionalCallback(err, response, body) {
+            if (!err && response.statusCode === 200) {
+                resolve(body);
+            } else {
+                resolve(null);
+            }
+        });
+    });
+};
+
 export const getDetails = (_req, _res, _next) => {
     _req.cData = {};
     return User.findOne({ _id: toObjectId(_req.params.id) })
-        .select('profile')
+        .select("profile email")
         .then(user => {
-            if(!user){
-                return _next(new Error('Volunteer cannot be found.'))
+            if (!user) {
+                return _next(new Error("Volunteer cannot be found."));
             } else {
                 _req.cData.user = user;
                 _next();
-            }   
+            }
         })
         .catch(err => _next(err));
 };
 
 export const getInitiatives = (_req, _res, _next) => {
-    
     return Initiative.find({
-            $or: [
-                { leader: toObjectId(_req.params.id) },
-                { members: { $elemMatch: { user: toObjectId(_req.params.id) } } }
-            ]
-        })
-        .select('name')
+        $or: [{ leader: toObjectId(_req.params.id) }, { members: { $elemMatch: { user: toObjectId(_req.params.id) } } }]
+    })
+        .select("name")
         .then(initiatives => {
-            _req.responseData = { 
-                user: _req.cData.user,
+            _req.responseData = {
+                user: { profile: _req.cData.user.profile },
+                time: _req.cData.time,
                 initiatives: initiatives
             };
             _next();
         })
         .catch(err => _next(err));
-    
-}
+};
+
+export const getTime = (_req, _res, _next) => {
+    return getInSyncData("/time", { email: _req.cData.user.email })
+        .then(data => {
+            _req.cData.time = data;
+            _next();
+        })
+        .catch(err => _next(err));
+};
+
+export const getScreenshots = (_req, _res, _next) => {
+    return getInSyncData("/screenshots", { email: _req.cData.user.email, page: _req.params.page })
+        .then(data => {
+            _req.responseData = { screenshots: data };
+            _next();
+        })
+        .catch(err => _next(err));
+};
