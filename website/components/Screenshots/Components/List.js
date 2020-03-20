627bb6ffe2bf4fbec228af6e8967813a048a698e
chano08 <christianraymondpalis@gmail.com>
2020-03-21 01:20:28 +0800

    hide screenshot items with no images

--- website/components/Screenshots/Components/List.js
+++ website/components/Screenshots/Components/List.js
@@ -19,6 +19,10 @@ const ScreenshotLists = ({ screenshots }) => {
         setActive(_img);
     };
 
+    const hasImage = screenshots => {
+        return screenshots.some(s => s.trim() !== '');
+    }
+
     useEffect(() => {
         document.addEventListener("keydown", escFunction, false);
 
@@ -35,34 +39,36 @@ const ScreenshotLists = ({ screenshots }) => {
                     <Row className="items">
                         {screenshots.map(screenshot => {
                             return (
-                                <Col className="item mb-3" sm="12" lg="4" key={screenshot._id}>
-                                    <Card>
-                                        <CardHeader>{moment(screenshot.start).format("LLL")}</CardHeader>
-                                        <CardBody>
-                                            {(() => {
-                                                if (screenshot.screenshots.length > 0) {
-                                                    return screenshot.screenshots.map(img => {
-                                                        return (
-                                                            <img
-                                                                className="pointer"
-                                                                onClick={() => {
-                                                                    showCarousel(img);
-                                                                }}
-                                                                key={img}
-                                                                src={img}
-                                                            />
-                                                        );
-                                                    });
-                                                } else {
-                                                    return <img src="/img/no-image-available.png" />;
-                                                }
-                                            })()}
-                                        </CardBody>
-                                        <CardFooter>
-                                            {screenshot.job.title} - {screenshot.task ? screenshot.task : "No Task"}
-                                        </CardFooter>
-                                    </Card>
-                                </Col>
+                                <React.Fragment>
+                                    {hasImage(screenshot.screenshots) && <Col className="item mb-3" sm="12" lg="4" key={screenshot._id}>
+                                        <Card>
+                                            <CardHeader>{moment(screenshot.start).format("LLL")}</CardHeader>
+                                            <CardBody>
+                                                {(() => {
+                                                    if (screenshot.screenshots.length > 0) {
+                                                        return screenshot.screenshots.map(img => {
+                                                            return (
+                                                                <img
+                                                                    className="pointer"
+                                                                    onClick={() => {
+                                                                        showCarousel(img);
+                                                                    }}
+                                                                    key={img}
+                                                                    src={img}
+                                                                />
+                                                            );
+                                                        });
+                                                    } else {
+                                                        return <img src="/img/no-image-available.png" />;
+                                                    }
+                                                })()}
+                                            </CardBody>
+                                            <CardFooter>
+                                                {screenshot.job.title} - {screenshot.task ? screenshot.task : "No Task"}
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>}
+                                </React.Fragment>
                             );
                         })}
                     </Row>
